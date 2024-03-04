d1a9333a47b827cadd38b79adc7547e109344362
Jass <frontendjass@gmail.com>
2024-03-04 10:34:37 -0600

    Implemented responsive navbar.

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,21 +1,44 @@
-import React from 'react';
-import styles from './Navbar.module.css';
+import React, { useState } from 'react';
+import styles from './Navbar.module.css'; 
 
 function Navbar() {
+  const [active, setActive] = useState(false); 
+
+  const handleActive = () => {
+    setActive(!active); 
+  }
+
   return (
     <header className={styles.navbarHeader}>
-        <nav className={styles.navbar}>
+      <nav className={styles.navbar}>
         <span className={styles.logo}>Jass</span>
-        <ul className={styles.navbarList}>
-            <li><a href="#home">Home</a></li>
-            <li><a href="#work">Work</a></li>
-            <li><a href="#testimonials">Testimonials</a></li>
-            <li><a href="#contact">Contact Me</a></li>
+        <ul className={`${styles.navbarList} ${active ? styles.active : 'active'}`}>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#work">Work</a></li>
+          <li><a href="#testimonials">Testimonials</a></li>
+          <li><a href="#contact">Contact Me</a></li>
         </ul>
-        </nav>
-    </header>
+        <div className={styles.hamburger} onClick={handleActive}>
+        {!active ? (
+          <>
+            <span className={styles.bar}></span>
+            <span className={styles.bar}></span>
+            <span className={styles.bar}></span>
+          </>
+        ) : (
+          <>
+            <span className={styles.activeBar}></span>
+            <span className={styles.activeBar}></span>
+            <span className={styles.activeBar}></span>
 
+          </>
+        )}
+         
+        </div>
+      </nav>
+    </header>
   );
 }
 
 export default Navbar;
+
