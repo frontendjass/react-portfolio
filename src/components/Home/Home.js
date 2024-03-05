d762db3945684edd3e820ed3d112237071d79fd6
Jass <frontendjass@gmail.com>
2024-03-05 10:34:37 -0600

    Implemented hero and fixed navbar background color.

---
+++ src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from './Home.module.css';
+
+function Home() {
+    return (
+        <div className={styles.hero}>
+            <h1>Elevating Brands through Exceptional Design</h1>
+        </div>
+    )
+}
+
+export default Home;
